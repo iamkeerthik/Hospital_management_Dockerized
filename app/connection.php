e3c15ecf21c68b6e9a68611a21758764aa4f8824
Ubuntu <ubuntu@ip-172-31-9-94.ap-south-1.compute.internal>
2023-09-02 15:38:02 +0000

    almost final

--- app/connection.php
+++ app/connection.php
@@ -1,3 +1,11 @@
 <?php
+// $host = 'terraform-20230823072829865900000001.cqjpi1fgxlzn.ap-south-1.rds.amazonaws.com';
+// $port = 3306;
+// $username = 'mysql_user';
+// $password = 'Q)vKw8X}GZ>e';
+// $dbname = 'hmsdb';
+
+// // Create a connection
+// $con = mysqli_connect($host, $username, $password, $dbname, $port);
 $con = mysqli_connect("db", "root", "mysql@123", "myhmsdb");
 ?>
